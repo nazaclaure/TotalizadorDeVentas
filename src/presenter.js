4f531cfe45f828dce9cf074c6640fb948f3d66e2
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-09-08 00:28:38 -0400

    feat: conectar interfaz de usuario con confirmacion, cancelacion y selector de estados

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,26 @@
-import sumar from "./sumador";
+import totalizador from "./totalizador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const cantidadInput = document.querySelector("#cantidad");
+const precioInput = document.querySelector("#precio");
+const estadoSelect = document.querySelector("#estado");
+const form = document.querySelector("#totalizador-form");
+const cancelarBtn = document.querySelector("#cancelar-btn");
+const divResultado = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const cantidad = Number(cantidadInput.value);
+  const precio = Number(precioInput.value);
+  const estado = estadoSelect.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const resultado = totalizador(cantidad, precio, estado);
+  divResultado.innerHTML = "<p>" + resultado + "</p>";
+});
+
+cancelarBtn.addEventListener("click", () => {
+  cantidadInput.value = "";
+  precioInput.value = "";
+  estadoSelect.selectedIndex = 0;
+  divResultado.innerHTML = "<p>Compra cancelada</p>";
 });
