9b9db2ce8d6d5938dd85d799f26b04922a271882
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-09-07 23:47:08 -0400

    fix: validar que la cantidad sea mayor a cero

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,4 +1,7 @@
 function totalizar(cantidad, precio) {
+  if (cantidad <= 0) {
+    return "Error: La cantidad debe ser mayor a 0";
+  }
   return cantidad * precio;
 }
 
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -4,4 +4,9 @@ describe("Totalizador de Ventas", () => {
   it("deberia calcular el precio neto base para 3 items a $10", () => {
     expect(totalizar(3, 10)).toEqual(30);
   });
+
+  it("deberia retornar mensaje de error si la cantidad es menor o igual a 0", () => {
+    expect(totalizar(-5, 10)).toEqual("Error: La cantidad debe ser mayor a 0");
+  });
+  
 });
